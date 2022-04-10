5f8952e40d2ee5fbbaf87d0efb4b6c798674753a
mahmoud shokry <mahmoud.shokry.engineer@gmail.com>
2022-04-10 09:41:55 +0200

    day1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,79 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
-function App() {
+export function TodoInput({onAdd}){
+  let [value, setValue] = useState("");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input type="text" value={value} onChange={(e) =>{setValue(e.target.value);}} />
+      <button
+       onClick ={ () => {
+        onAdd(value);
+        setValue(""); 
+       }
+       }
+       >
+        Add todo 
+      </button>
     </div>
   );
 }
 
-export default App;
+export function TodoList ({items,onDelete}){
+  return (
+    <ul>
+      {items.map((item)=>(
+        <TodoItem
+        key={item.id}
+        value={item}
+        onDelete={() =>{
+          onDelete(item);
+        }
+        }
+        /> 
+
+      )
+
+      )
+      }
+    </ul>
+  );
+}
+
+export function TodoItem ({value,onDelete}) {
+  return(
+    <li>
+      <input type="checkbox" checked={value.completed} />
+      {value.label}
+      <button onClick={() => onDelete()}>X</button>
+    </li>
+  );
+}
+
+ export function App() {
+  const [items,setItems] = useState([
+{id: 1, label:"Item1", completed:true},
+{id: 2, label:"Item2",completed:false},
+  ]);
+  return(
+    <div>
+      <TodoInput
+      onAdd={(value) =>{
+        setItems([
+          ...items,
+          {id:Math.random(),label:value,completed :false},
+        ])
+      }
+      }
+      />
+      <TodoList items={items} onDelete={(item) =>{
+setItems(items.filter((todoItem) => todoItem.id !== item.id ))
+      }}/>
+    </div>
+  );
+  
+} 
+
+
+
